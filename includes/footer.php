f0f48343b88f0c2fe93a93aea2b7bd2d725b0999
np03cs4a240158-blip <np03cs4a240158@heraldcollege.edu.np>
2026-04-03 08:22:17 +0545

    Initial commit - backend setup

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<?php
+// includes/footer.php
+// Closes out the body layout and attaches necessary scripts
+?>
+    <!-- Load shared scripts (Path relative to public directory) -->
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
